f832a6157f1810b9abd8613efb3aa7023773cb0a
Irabor Nosa <nosakhare@esettlementgroup.com>
2023-06-18 01:14:14 +0100

    coming soon text

--- src/components/Symbols.tsx
+++ src/components/Symbols.tsx
@@ -19,13 +19,7 @@ const Symbols = () => {
             rounded-lg border-grey max-w-[700px] space-y-4 text-[20px] p-12"
         >
         <p className="opacity-[.85]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Coming Soon...
         </p>
       </div>
     </div>
