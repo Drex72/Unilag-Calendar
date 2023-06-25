8e88da72f068e9019e7561a0aea604f3f7b04152
tobySolutions <adedejitobiloba7@gmail.com>
2023-06-25 12:12:48 +0100

    re-organize and setup section routing

--- src/components/SectionII/DistinguishedProfessors.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function DistinguishedProfessors() {
-  return (
-    <div>DistinguishedProfessors</div>
-  )
-}
-
--- src/components/SectionII/Senate.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function Senate() {
-  return (
-    <div>Senate</div>
-  )
-}
-
