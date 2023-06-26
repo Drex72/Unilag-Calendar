7e96c705ded3ac9697e04fb82c3984882c8ab454
Drex <femidavooo@gmail.com>
2023-06-26 11:21:36 +0100

    made changes

--- src/components/SectionVI/SectionVI.tsx
+++ src/components/SectionVI/SectionVI.tsx
@@ -21,61 +21,134 @@ const SectionVI = () => {
                 <div>
                     <p>
                     Admission of Students
-<br/>
-<p>
-Application for admission into first degree courses is made through the Joint Admissions and Matriculation Board (JAMB) which has the responsibility for admitting students into first degree courses in Nigerian Universities. Usually, the advertisement for admission will appear in the national newspapers preceding the year of admission. Such applications should be addressed to:
-<br/>
-National Secretariat								Lagos Area Office
+                        <br/>
+                        <p>
+                        Application for admission into first degree courses is made through the Joint Admissions and Matriculation Board (JAMB) which has the responsibility for admitting students into first degree courses in Nigerian Universities. Usually, the advertisement for admission will appear in the national newspapers preceding the year of admission. Such applications should be addressed to:
+                        <br/>
+                        National Secretariat								Lagos Area Office
 
-The Registrar,									11, Ojora Road, lkoyi,
+                        The Registrar,									11, Ojora Road, lkoyi,
 
-Suleja Road, Bwari,								PM.B. 12748.
+                        Suleja Road, Bwari,								PM.B. 12748.
 
-PM.B. 189, Garki,								Lagos.
+                        PM.B. 189, Garki,								Lagos.
 
-Abuja.
-</p>
-<br/>
-<p>
-Candidates for the diploma and certificate courses are required to complete an application form obtained from the Human Resources Development Centre of the University. For higher degree courses applications should be made through the Post-Graduate School.
-</p>
-<br/>
-<p>
-Completed forms for diploma and certificate courses as well as for higher degrees are normally submitted not later than the April preceding the October in which the applicant wishes to enter.
-</p>
-<br/>
-<p>
-Direct entry and UTME and candidates are required to have reached the age of sixteen (16) years on or before the first day of October in the year of their admission. 
-</p>
-<br/>
-<p>
-For admission to all degree courses, candidates must satisfy the entry requirements of the University. For certain degree courses, additional faculty or departmental requirements may have to be satisfied.
-</p>
-<br/>
-<p>
-Upon admission, students shall pay to the University the fees prescribed by regulations, and register as students of the University.
-</p>
-<br/>
-<p>
+                        Abuja.
+                        </p>
+                        <br/>
+                        <p>
+                        Candidates for the diploma and certificate courses are required to complete an application form obtained from the Human Resources Development Centre of the University. For higher degree courses applications should be made through the Post-Graduate School.
+                        </p>
+                        <br/>
+                        <p>
+                        Completed forms for diploma and certificate courses as well as for higher degrees are normally submitted not later than the April preceding the October in which the applicant wishes to enter.
+                        </p>
+                        <br/>
+                        <p>
+                        Direct entry and UTME and candidates are required to have reached the age of sixteen (16) years on or before the first day of October in the year of their admission. 
+                        </p>
+                        <br/>
+                        <p>
+                        For admission to all degree courses, candidates must satisfy the entry requirements of the University. For certain degree courses, additional faculty or departmental requirements may have to be satisfied.
+                        </p>
+                        <br/>
+                        <p>
+                        Upon admission, students shall pay to the University the fees prescribed by regulations, and register as students of the University.
+                        </p>
+                        <br/>
+                        <p>
 
-Applicants who are offered places in the University will be required to submit birth certificates or other acceptable evidence of age and documentary evidence of examination qualifications for scrutiny in the Admissions Office.
-</p>
-<br/>
-<p>
-A candidate may be required to attend the University for interview or for such tests as may be set to assess suitability for admission to a particular course.
-</p>
-<br/>
-<p>
-The University reserves the right to accept or reject an application for admission to any particular course of study.
-</p>
-<br/>
+                        Applicants who are offered places in the University will be required to submit birth certificates or other acceptable evidence of age and documentary evidence of examination qualifications for scrutiny in the Admissions Office.
+                        </p>
+                        <br/>
+                        <p>
+                        A candidate may be required to attend the University for interview or for such tests as may be set to assess suitability for admission to a particular course.
+                        </p>
+                        <br/>
+                        <p>
+                        The University reserves the right to accept or reject an application for admission to any particular course of study.
+                        </p>
+                        <br/>
 
                     </p>
                 </div>
             </Element>
 
-            <Element name="regulations-governing-first-degree">
-                <TextHeading content='Regulations Governing First Degree Programmes.' />
+            <Element name="ENTRY-REQUIREMENTS-FOR-DEGREE-PROGRAMMES">
+                <TextHeading content='ENTRY, REQUIREMENTS FOR DEGREE PROGRAMMES' />
+                {/* Content Here */}
+                <div>
+                    <p>
+                    <br/>
+                        1.	University Tertiary Matriculation Examination (UTME)
+                        <br/>
+                        <p>
+                        Admission is through the University Tertiary Matriculation Examination and Direct Entry to all Faculties. Minimum entry qualifications for admissions through the Joint Matriculation Examination will be one of the following:
+                        </p>
+                        <br/>
+                        <p>
+                        West African School Certificate, G.C.E. (A’ Level) or Senior Secondary School Certificate with credit in at least five subjects, including English Language and Mathematics.
+                        </p>
+                        <br/>
+                        <p>
+
+                        2.	Direct Entry 
+                        </p>
+                        <br/>
+                        <p>
+
+                        i.	First degrees of university recognized by Senate for this purpose. 
+                        ii.	General Certificate of Education (A’ Level), Joint Universities Preliminary Examination Board (JUPEB), Cambridge A’ Level with passes in three (3) relevant subjects.
+                        iii.	Candidates with NCE are considered for admission only in the Faculties of Education and Arts provided that in addition to their NCE, they must have obtained credit passes in five (5) relevant O’/Level subjects (English and Mathematics inclusive) 
+                        </p>
+                        <br/>
+                        <p>
+
+                        ADDITIONAL INFORMATION
+                        </p>
+                        <br/>
+                        <p>
+
+                        (a)	Direct entry candidates for admission into the Faculty of Engineering must have a credit pass in Further Mathematics at O’level. 
+                        </p>
+                        <p>
+
+                        (b)	Candidates for B.Sc. programme in Faculty of Engineering must have good passes in Mathematics, Physics and Chemistry at JUPEB/Cambridge A’Level.  Candidates with HND/OND in relevant fields are also considered for admission.
+                        *	Surveying and Geoinformatics accepts certificate from Federal School of Surveying.
+                        *	Candidates for B.Sc. programme in Biomedical Engineering must have six O’level credit passes in English, Mathematics, further mathematics, Biology, Physics and Chemistry. 
+                        </p>
+                        <p>
+
+                        (c)	Candidates for admission to degree courses in Faculty of Management Sciences must possess very good passes in three relevant JUPEB/A’Level subjects.
+                        </p>
+                        <p>
+
+                        (d)	Candidates for degree courses in Sociology and Social Work should note that the Department   accepts University of Lagos Diploma in Social Development and Administration (SDA)
+                        </p>
+                        <p>
+
+                        (e)	Candidates for degree course in Psychology must have a credit pass in biology at O’Level.
+                        </p>
+                        <p>
+
+                        (f)	Direct Entry candidates for degree programmes in Computer Science, Mathematics, Statistics, Physics and Industrial Mathematics must have a credit pass in Further Mathematics at O’Level.
+                        </p>
+                        <p>
+
+                        (g)	Candidates for admission by Direct Entry to the College of Medicine must have a pass in at least three science subjects, namely, Physics, Chemistry, Biology. Advanced Level Examination. In addition, candidates must have passed English Language and Mathematics at the G.C.E. Ordinary Level. 
+                        </p>
+                        <p>
+
+                        (h)	Candidates for B.Sc. degree into College of Medicine must have good passes in Physics, Chemistry and Biology at JUPEB/A’Level.
+                        </p>
+                        <br/>
+
+                    </p>
+                </div>
+            </Element>
+
+            <Element name="regulations-governing-first-degree-courses">
+                <TextHeading content='Regulations Governing First Degree Courses.' />
                 {/* Content Here */}
                 <div>
                     <p>
@@ -83,7 +156,6 @@ The University reserves the right to accept or reject an application for admissi
                     </p>
                 </div>
             </Element>
-
             <Element name="regulations-governing-exam-conduct">
                 <TextHeading content='Regulations Governing the Conduct of Examinations.' />
                 {/* Content Here */}
