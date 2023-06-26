adba8250be7069f8276d50099497995828dc7a21
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-26 16:59:01 +0100

    Creating  new SectionVII

---
+++ src/components/SectionVII/AcademicUnit.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from "../Layout/MainLayout";
+import TextHeading from "../TextHeading/TextHeading";
+
+export function AcademicUnit() {
+    return (
+        <MainLayout>
+            <div>
+                <h1 className="">
+                    ACADEMIC UNITS IN THE SCHOOL, COLLEGE AND FACULTIES
+                </h1>
+            </div>
+        </MainLayout>
+    );
+}
