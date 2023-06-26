41809676738f7d143cd081e34ea729b2fba1aec4
Drex <femidavooo@gmail.com>
2023-06-26 10:45:49 +0100

    made changes

--- src/components/SectionVI/SectionVI.tsx
+++ src/components/SectionVI/SectionVI.tsx
@@ -9,32 +9,99 @@ const SectionVI = () => {
                 <TextHeading content='General Regulations' />
                 {/* Content Here */}
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quam laudantium? Ipsam nulla maxime saepe voluptatem temporibus possimus deserunt impedit beatae sequi officiis quae error est delectus provident quos recusandae, maiores neque tempora porro. Soluta possimus consectetur cupiditate cumque laudantium rem sapiente dolor perspiciatis omnis maxime nulla magni error ab, obcaecati expedita molestias ducimus quasi eaque quibusdam amet vero, eos explicabo asperiores. Illum, vitae debitis atque praesentium, numquam dolorum saepe temporibus incidunt quam ad velit provident nulla iure veritatis recusandae necessitatibus consequuntur ab minus itaque, magni quasi laudantium repudiandae. Esse eligendi suscipit tempore libero consequatur, corrupti labore dolores mollitia? Exercitationem.</p>
+                    <p>
+
+                    </p>
                 </div>
             </Element>
 
             <Element name="admission-requirements">
                 <TextHeading content='Admission Requirements' />
                 {/* Content Here */}
-                <div></div>
+                <div>
+                    <p>
+                    Admission of Students
+<br/>
+<p>
+Application for admission into first degree courses is made through the Joint Admissions and Matriculation Board (JAMB) which has the responsibility for admitting students into first degree courses in Nigerian Universities. Usually, the advertisement for admission will appear in the national newspapers preceding the year of admission. Such applications should be addressed to:
+<br/>
+National Secretariat								Lagos Area Office
+
+The Registrar,									11, Ojora Road, lkoyi,
+
+Suleja Road, Bwari,								PM.B. 12748.
+
+PM.B. 189, Garki,								Lagos.
+
+Abuja.
+</p>
+<br/>
+<p>
+Candidates for the diploma and certificate courses are required to complete an application form obtained from the Human Resources Development Centre of the University. For higher degree courses applications should be made through the Post-Graduate School.
+</p>
+<br/>
+<p>
+Completed forms for diploma and certificate courses as well as for higher degrees are normally submitted not later than the April preceding the October in which the applicant wishes to enter.
+</p>
+<br/>
+<p>
+Direct entry and UTME and candidates are required to have reached the age of sixteen (16) years on or before the first day of October in the year of their admission. 
+</p>
+<br/>
+<p>
+For admission to all degree courses, candidates must satisfy the entry requirements of the University. For certain degree courses, additional faculty or departmental requirements may have to be satisfied.
+</p>
+<br/>
+<p>
+Upon admission, students shall pay to the University the fees prescribed by regulations, and register as students of the University.
+</p>
+<br/>
+<p>
+
+Applicants who are offered places in the University will be required to submit birth certificates or other acceptable evidence of age and documentary evidence of examination qualifications for scrutiny in the Admissions Office.
+</p>
+<br/>
+<p>
+A candidate may be required to attend the University for interview or for such tests as may be set to assess suitability for admission to a particular course.
+</p>
+<br/>
+<p>
+The University reserves the right to accept or reject an application for admission to any particular course of study.
+</p>
+<br/>
+
+                    </p>
+                </div>
             </Element>
 
             <Element name="regulations-governing-first-degree">
                 <TextHeading content='Regulations Governing First Degree Programmes.' />
                 {/* Content Here */}
-                <div></div>
+                <div>
+                    <p>
+
+                    </p>
+                </div>
             </Element>
 
             <Element name="regulations-governing-exam-conduct">
                 <TextHeading content='Regulations Governing the Conduct of Examinations.' />
                 {/* Content Here */}
-                <div></div>
+                <div>
+                    <p>
+
+                    </p>
+                </div>
             </Element>
 
             <Element name="regulations-governing-graduate-studies">
                 <TextHeading content='Regulations Governing Graduate Studies.' />
                 {/* Content Here */}
-                <div></div>
+                <div>
+                    <p>
+
+                    </p>
+                </div>
             </Element>
 
         </MainLayout>
