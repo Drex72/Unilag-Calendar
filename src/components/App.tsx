37a4dcf0ec0ef1787a0ce89618d24df61e2d700c
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-27 20:32:54 +0100

    Adding COmponents from Section 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,6 +11,20 @@ import Symbols from "./Symbols";
 import Home from "./Home";
 import History from "./SectionI/History";
 import Development from "./SectionI/Development";
+import AcademicUnit from "./SectionVII/AcademicUnit";
+import Arts from "./SectionVII/arts";
+import CmulBASIC from "./SectionVII/cmul-basic";
+import CmulCLINICAL from "./SectionVII/cmul-clinical";
+import CmulDENTAL from "./SectionVII/cmul-dental";
+import Education from "./SectionVII/education";
+import Engineering from "./SectionVII/engineering";
+import Environmental from "./SectionVII/environmental";
+import Law from "./SectionVII/law";
+import ManagementSCIENCE from "./SectionVII/management-science";
+import Pharmacy from "./SectionVII/pharmacy";
+import Science from "./SectionVII/science";
+import SocialSCIENCE from "./SectionVII/social-science";
+import SPGS from "./SectionVII/spgs";
 import ScrollToTop from "./ScrollToTop/ScrollToTop";
 import { SectionII } from "./SectionII/";
 import HousingUnit from "./SectionXi/HousingUnit";
@@ -53,6 +67,24 @@ function App() {
             <Route path="/sectionVIII" element={<SectionVIII />} />
             <Route path="/sectionIX" element={<SectionIX />} />
             <Route path="/sectionX" element={<SectionX />} />
+
+            
+            <Route path="/sectionVII" element={<SectionVII />} />
+            <Route path="/sectionVII/academic-unit" element={<AcademicUnit />} />
+            <Route path="/sectionVII/i" element={<Arts/>} />
+            <Route path="/sectionVII/ii" element={<CmulBASIC />} />
+            <Route path="/sectionVII/iii" element={<CmulCLINICAL />} />
+            <Route path="/sectionVII/iv" element={<CmulDENTAL />} />
+            <Route path="/sectionVII/v" element={<Education />} />
+            <Route path="/sectionVII/vi" element={<Engineering />} />
+            <Route path="/sectionVII/vii" element={<Environmental />} />
+            <Route path="/sectionVII/viii" element={<Law />} />
+            <Route path="/sectionVII/ix" element={<ManagementSCIENCE />} />
+            <Route path="/sectionVII/x" element={<Pharmacy />} />
+            <Route path="/sectionVII/xi" element={<Science />} />
+            <Route path="/sectionVII/xii" element={<SocialSCIENCE />} />
+            <Route path="/sectionVII/xiii" element={<SPGS />} />
+
 
             <Route path="/sectionXI/i" element={<HousingUnit />} />
             <Route path="/sectionXI/ii" element={<MedicalCenter />} />
--- src/components/SectionVII/cmul-clinical.tsx
+++ src/components/SectionVII/cmul-clinical.tsx
@@ -1,29 +1,32 @@
-
-import React from 'react';
-import MainLayout from '../Layout/MainLayout';
-import TextHeading from '../TextHeading/TextHeading';
+import React from "react";
+import MainLayout from "../Layout/MainLayout";
+import TextHeading from "../TextHeading/TextHeading";
 import { Element } from 'react-scroll';
 import { useNavigate } from 'react-router-dom'
 
-export function AcademicUnit() {
+export function CmulCLINICAL() {
+
     const navigate = useNavigate()
     return (
         <MainLayout>
-            <Element name='AcademicUnit'>
-                <TextHeading content='ACADEMIC UNITS IN THE SCHOOL, COLLEGE AND FACULTIES.' />
+            <Element name='CMUL-CLINICAL'>
+                <TextHeading content='Faculty of Clinical Sciences (CMUL).' />
                 <p>
                 The Faculties and Departments provide a window into the academic programmes of the University of Lagos. Their evolution also directly mirrors the physical and academic development of the University since inception. Currently, the University has 12 Faculties, 94 Departments, 88 programmes in addition to the School of Postgraduate Studies. A brief history of each is provided below:
                 </p>
-            </Element>
+            </Element> 
+
+
 
 
             <div className='button-container'>
+                    <button onClick={() => navigate('/sectionVII/ii')}>Previous  - Faculty Of Basic Medical Sciences</button>
 
-                    <button onClick={() => navigate('/sectionVII/ii')}>Next  - Faculty Of Basic Medical Sciences</button>
+                    <button onClick={() => navigate('/sectionVII/iv')}>Next  - Faculty Of Dental Sciences</button>
                 </div>
         </MainLayout>
     );
 }
 
 
-export default AcademicUnit
+export default CmulCLINICAL
